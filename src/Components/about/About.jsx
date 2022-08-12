2eda7583e139e2198ce5a069186bb7f575a8bb29
abinadiS <abinadiesteven@gmail.com>
2022-08-11 23:49:40 -0500

    Arreglando estillos en about

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -8,11 +8,7 @@ const About = () => {
             <h3 className="about__title">Permiteme contarte sobre mi</h3>
             <p className="about__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate sit, distinctio veniam sit, distinctio veniam quisit, distinctio veniam qui quisquam rtenetur ipsaessitatibus maxime.</p>
         </div>
-        <div className="about__imagen__container">
-
-        <img src={aboutimagen} alt="" className="about__imagen"/>
-
-        </div>
+       <img src={aboutimagen} alt="about" className='about__imagen'/>
         
 
     </div> );
