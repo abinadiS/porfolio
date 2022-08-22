7b8f27848b4da212dba9165f228bc4080a8878a6
abinadiS <abinadiesteven@gmail.com>
2022-08-21 22:25:03 -0500

    creacion de componente y css de skill

---
+++ src/Components/Skills/Skills.jsx
@@ -0,0 +1,9 @@
+import "./Skills.css";
+const Skills = () => {
+    return ( 
+    <div className="skills__container">
+        <h1 className="skill__title">Conocimientos</h1>
+    </div> );
+}
+ 
+export default Skills;
