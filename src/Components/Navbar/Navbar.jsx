7b2d25029f4378fb53521d7bc11da15d7b6b3747
abinadiS <abinadiesteven@gmail.com>
2022-08-23 21:03:38 -0500

    scrooling detected

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import "./Navbar.css";
 
-const Navbar = ({scrollHeight}) => {
+const Navbar = ({ scrollHeight }) => {
 
-    const toTheTop = () =>{
-        window.scrollTo({top: 0, left: 0, behavior: "smooth"})
+    const toTheTop = () => {
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
     }
-
-    
-    return ( 
+    return (
         <div className={`navbar ${scrollHeight > 20 ? "scrolling" : null}`}>
             <div className="navbar__logo" onClick={toTheTop}>Abinadi</div>
         </div>
-     );
+    );
 }
- 
+
 export default Navbar;
