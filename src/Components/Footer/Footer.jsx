d1c90f72212c694b3b55d497ff4a576572cb423e
abinadiS <abinadiesteven@gmail.com>
2022-08-23 23:07:45 -0500

    creacion del footer con estilos

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+const Footer = () => {
+    return ( 
+        <div className="footer__container">
+            <p>Todos los derechos reservados</p>
+            <p className="footer__diseñado">Diseñado por Abinadi Contreras</p>
+        </div>
+     );
+}
+ 
+export default Footer;
