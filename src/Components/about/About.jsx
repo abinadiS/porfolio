1291e2853476e2f43bd519f5b6b2767db806c2e6
abinadiS <abinadiesteven@gmail.com>
2022-08-24 22:37:36 -0500

    Actualizando sobre mi y mejorando RWD

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -6,7 +6,8 @@ const About = () => {
         <div className="about__container">
             <div className="about__contenido">
                 <h3 className="about__title">Permiteme contarte sobre mi</h3>
-                <p className="about__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate sit, distinctio veniam sit, distinctio veniam quisit, distinctio veniam qui quisquam rtenetur ipsaessitatibus maxime.</p>
+                <p className="about__description">Desarrollador Frontend con conocimientos en React desarrollado a travéz de proyectos propios.
+                Busco mejorar mis habilidades, aprendiendo cada dia más para lograr crecer en la industria.  </p>
             </div>
             <img src={aboutimagen} alt="about" className='about__imagen' />
 
